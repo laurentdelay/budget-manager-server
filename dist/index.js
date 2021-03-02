bdc13a120dbf511e2ec5367dc2b59eade2553e46
laurentdelay <laurent.delay89@gmail.com>
2021-03-02 19:22:32 +0100

    correction .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const envVars_1 = require("./utils/envVars");
-server_1.startServer(envVars_1.PORT).catch((error) => {
-    console.error(error.message);
-});
-//# sourceMappingURL=index.js.map
--- dist/utils/token.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET_KEY = void 0;
-exports.SECRET_KEY = process.env.SECRET_KEY ?? "ultrasuperdupersecretkey";
-//# sourceMappingURL=token.js.map
